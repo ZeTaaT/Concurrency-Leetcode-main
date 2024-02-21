049841156b7a2d10e11e69c560f9e3c0697ec8aa
ZeTaaT <illiaappa@gmail.com>
2024-02-21 23:51:19 +0000

    Basic Func done...... hmmmmmm

--- app/Consumer.py
+++ app/Consumer.py
@@ -1,14 +1,22 @@
 from collections import deque
+from bs4 import BeautifulSoup
 from Producer import Producer
+from queue import Queue
 class Consumer:
 
-    htmlQueue = []
+    htmlQueue = deque()
 
     def __init__(self):
         self.htmlQueue = deque() #good way of storing data as queue
 
-    def readQueue(self):
+    def readQueue(self, queue: Queue):
+        while not queue.empty():
+            self.htmlQueue.append(self.extractHyper(queue.get()))
         print("code")
 
+    def extractHyper(self, soup: BeautifulSoup):
+        for link in soup.find_all('a'): 
+           print(link.get('href'))   
+
     def startWorking(self, prod: Producer):
-        print("AAAAA")
+        self.readQueue(prod.queue)
