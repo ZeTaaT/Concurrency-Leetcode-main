19500bbccecce4ad1c159e5f9d53ed64814ac6b1
ZeTaaT <illiaappa@gmail.com>
2024-02-21 21:04:47 +0000

    Can extract data

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ Leetcode is so cool, I lolve Leecode coding challenges.
 # How to make it work
 
 Step 1: Donwload the latest version of Python
-Step 2: Run "pip install -r requirements.txt" 
+Step 2: Run "python -m pip install -r requirements.txt" 
--- app/Producer.py
+++ app/Producer.py
@@ -1,4 +1,7 @@
 from queue import Queue
+from bs4 import BeautifulSoup
+import requests 
+import re 
 
 class Producer:
     queue = Queue()
@@ -7,17 +10,42 @@ class Producer:
         self.queue = Queue(maxsize=10000) #queue from queue are more effecient than deque becuase of threading communications
 
     def readURL(self, dataPath: str): #read URLs from the list file
+
         print("Trying to read the data")
         try:
             file = open(dataPath, 'r')
-            url = file.readline()
+            url = file.readline().strip() #Strip for the empty spaces
+            print(url)
             return url
         except Exception as e:
-            print(e)
+            print("Error while reading data" + e)
 
-    def extractMarkup(self, url: str): #Extract Markup from the queue. wtf is a markup?
+    def requestHTML(self, url: str):
+        try:
+            response = requests.get(url) 
+
+            if response.status_code == 200:
+                print('Success!')
+            elif response.status_code == 404:
+                print('Not Found.')
+
+            return response
+        except Exception as e:
+            print("Error while fetching website HTML:", e)
+
+    def makeSoup(self, html_document):
+        try:
+            soup = BeautifulSoup(html_document.content, 'html.parser') 
+            for link in soup.find_all('a',  
+                          attrs={'href': re.compile("^https://")}): 
+                # display the actual urls 
+                print(link.get('href'))   
+        except Exception as e:
+            print("Error while making soup:", e)
         
-        print(url)
+    def extractMarkup(self, url: str): #Extract Markup from the queue. wtf is a markup?
+        html_document = self.requestHTML(url)
+        self.makeSoup(html_document)
 
     def startWorking(self, dataPath: str):
         url = self.readURL(dataPath)
