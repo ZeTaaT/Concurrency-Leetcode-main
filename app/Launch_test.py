3670d45e303e1caa6ec0f2f27c80044315396a79
ZeTaaT <illiaappa@gmail.com>
2024-02-22 10:52:38 +0000

    Finally working async tests

--- app/Launch_test.py
+++ app/Launch_test.py
@@ -5,19 +5,32 @@ from Producer import Producer
 
 #Testing all the processes in the project
 
-class TestStringMethods(unittest.TestCase): 
+events=[]
+
+class Test(IsolatedAsyncioTestCase): 
+      
+    def setUp(self): 
+        events.append("setUp")
+        pass
 
     #Checks if the fake file exists in the current context
     def test_test_file_exists(self): 
         dataPath = "../fakeData/testData1.txt"
-        self.assertTrue(exists(dataPath)) 
+        self.assertEqual(exists(dataPath), True) 
 
 
     #Checks if the test file exists in the current context
     def test_fake_file_exists(self): 
         dataPath = "../fakeData/fakeData1.txt"
-        self.assertTrue(exists(dataPath)) 
+        self.assertEqual(exists(dataPath), True) 
+
+    async def test_producer_works(self):
+        events.append("test_response")
+        dataPath = "../fakeData/testData1.txt"
+        test_prod = Producer()
+        await test_prod.startWorking(dataPath)
+        self.assertEqual(not test_prod.queueHtml.empty(), True) 
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main() 
