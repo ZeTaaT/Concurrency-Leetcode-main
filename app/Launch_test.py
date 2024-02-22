85092dc0ab0cfaba3cc15967c966d50ef7a84189
ZeTaaT <illiaappa@gmail.com>
2024-02-22 10:51:43 +0000

    Changed name for tests

--- app/Launch_test.py
+++ app/Launch_test.py
@@ -5,25 +5,19 @@ from Producer import Producer
 
 #Testing all the processes in the project
 
-events=[]
-
-class Test(IsolatedAsyncioTestCase): 
-      
-    def setUp(self): 
-        events.append("setUp")
-        pass
+class TestStringMethods(unittest.TestCase): 
 
     #Checks if the fake file exists in the current context
-    async def test_file_exists(self): 
+    def test_test_file_exists(self): 
         dataPath = "../fakeData/testData1.txt"
-        self.assertEqual(exists(dataPath), True) 
+        self.assertTrue(exists(dataPath)) 
 
 
     #Checks if the test file exists in the current context
-    async def fake_file_exists(self): 
+    def test_fake_file_exists(self): 
         dataPath = "../fakeData/fakeData1.txt"
-        self.assertEqual(exists(dataPath), True) 
+        self.assertTrue(exists(dataPath)) 
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     unittest.main() 
