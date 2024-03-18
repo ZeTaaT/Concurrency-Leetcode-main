28caa5ede4b8dbb2c208bea7847f723b5b8fa5ca
ZeTaaT <illiaappa@gmail.com>
2024-03-18 00:25:29 +0000

    Improved producer error handling

--- app/Producer.py
+++ app/Producer.py
@@ -11,38 +11,39 @@ class Producer: #Collect Html data
         self.queueData = Queue(maxsize=10000) #queue from queue are more effecient than deque becuase of threading communications, although am not using threads
 
     async def readURL(self, dataPath: str): #read URLs from the list file
-
         try:
             with open(dataPath, 'r') as file:
                 for url in file:
                     url = url.strip() #Strip for the empty spaces
                     await self.extractMarkup(url)
         except Exception as e:
-            print("Error while reading data", e)
+            print("Error while reading data", e) #Thrown when the file doesn't exist
 
-    async def requestHTML(self, url: str): #Get HTML from the URL
+    async def requestHTML(self, url: str) -> requests.Response: #Get HTML from the URL
         try:
-            response = requests.get(url) 
-            if response.status_code == 200:
-                return response
-            elif response.status_code == 404:
-                return ""
-        except Exception as e:
-            print("Error while fetching website HTML:", e)
+            response = requests.get(url, timeout = 1) 
+            return response
+        except Exception as e: 
+            print("Error while getting html:", e)
+            response = requests.Response()
+            response.status_code = 500
+            response._content = ""
+            response.reason = e
+            return response
+
 
     async def makeSoup(self, html_document): #Make the HTML into a soup(More ordered HTML that is used in the future to sort out markups)
         try:
-            soup = BeautifulSoup(html_document.content, 'html.parser') 
-            return soup
+            return BeautifulSoup(html_document.content, 'html.parser') 
         except Exception as e:
             print("Error while making soup:", e)
-            return ""
+            return BeautifulSoup("", "html.parser")
         
     async def extractMarkup(self, url: str): #Extract Markup from the URL.
         html_document = await self.requestHTML(url)
-        soup = await self.makeSoup(html_document)
-        if(soup != ""): #If the HTML not empty, put html into a queue as well as the url it was taken from
-            self.queueData.put((url, soup))
+        if(html_document.ok):
+            soup = await self.makeSoup(html_document)
+            self.queueData.put((url, soup)) #Even if the HTML empty, put html into a queue as well as the url it was taken from
         await asyncio.sleep(0.001)
 
     async def startWorking(self, dataPath: str): #Start the Producer
